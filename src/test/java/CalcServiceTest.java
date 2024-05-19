1e6766f55473824141e3aefab2af4e6b116fd1e8
Andrew Belokopytov <kopitinzp81@gmail.com>
2024-05-19 12:51:21 +0300

    add CalcService

---
+++ src/test/java/CalcServiceTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalcServiceTest {
+    @Test
+    void shouldCalcThree() {
+        CalcService service = new CalcService();
+
+        int income = 10000;     // Доход от работы
+        int expenses = 3000;    // Обязательные месячные траты
+        int threshold = 20000;  // Затраты на отдых
+        int expected = 3;        // Планируемое количество месяцев отдыха
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalcTwo() {
+        CalcService service = new CalcService();
+
+        int income = 100000;    // доход от работы
+        int expenses = 60000;   // обязательные месячные траты
+        int threshold = 150000; // траты на отдых
+        int expected = 2;        // ожидаемое количество месяцев отдыха
+
+        int actual = service.calculate(income, expenses, threshold);
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
